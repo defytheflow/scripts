e4f480dd4632dbae173a5351ba2ce1407872fcc8
Artyom Danilov <defytheflow@gmail.com>
2020-02-29 13:58:23 +0300

    'detab.c' and 'entab.c' - refactored programs and added options.

--- detab.c
+++ detab.c
@@ -1,18 +1,65 @@
+/*
+ * Compile with: gcc -o detab detab.c
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-#define TAB 8
+#define HELP_MSG "Try 'detab -h' for more information."
 
-int main(void)
+void usage()
 {
-	char c;
+        puts("Converts tabs to spaces.\n");
 
-	while ((c = (char) getchar()) != EOF) {
-		if (c == '\t')
-			for (int i = 0; i < TAB; ++i)
-			putchar(' ');
-		else
-			putchar(c);
-	}
+        puts("Usage:");
+        puts("  detab [options]\n");
 
-	return 0;
+        puts("Options:");
+        puts("  -h           show this message.");
+        puts("  -t <size>    number of spaces per tab.\n");
+}
+
+void parse_opts(int argc, char** argv, int* tab_out)
+{
+        char c;
+        opterr = 0;
+
+        while ((c = getopt(argc, argv, "ht:")) != -1)
+            switch (c) {
+              case 'h':
+                  usage();
+                  exit(0);
+              case 't':
+                *tab_out = atoi(optarg);
+                break;
+              case '?':
+                  fprintf(stderr, "Error: ");
+                  if (optopt == 't')
+                      fprintf(stderr, "option -'%c' requires an argument.\n",
+                              optopt);
+                  else
+                      fprintf(stderr, "unknown option '-%c'.\n",
+                              optopt);
+                  fprintf(stderr, "%s\n", HELP_MSG);
+                  exit(1);
+            }
+}
+
+int main(int argc, char** argv)
+{
+        char c;
+        int tab = 8;
+
+        parse_opts(argc, argv, &tab);
+
+        while ((c = (char) getchar()) != EOF) {
+                if (c == '\t')
+                        for (int i = 0; i < tab; ++i)
+                        putchar(' ');
+                else
+                        putchar(c);
+        }
+
+        return 0;
 }
--- entab.c
+++ entab.c
@@ -1,35 +1,80 @@
+/*
+ * Compile with: gcc -o entab entab.c -lio.
+ * io library can be found at 'https://github.com/defytheflow/lib.git'
+ */
 #include <io.h>
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-#define TAB 8
-#define MAX_LINE 256
+#define MAX_LINE 512
+#define HELP_MSG "Try 'entab -h' for more information."
 
-int check_next(char line[], int index, int count, char c)
+void usage()
 {
-        for (int i = 0; i < count; ++i, ++index) {
-                if (line[index] != c)
+        puts("Converts spaces to tabs.\n");
+
+        puts("Usage:");
+        puts("  entab [options]\n");
+
+        puts("Options:");
+        puts("  -h           show this message.");
+        puts("  -t <size>    number of spaces per tab.\n");
+}
+
+void parse_opts(int argc, char** argv, int* tab_out)
+{
+        char c;
+        opterr = 0;
+
+        while ((c = getopt(argc, argv, "ht:")) != -1)
+            switch (c) {
+              case 'h':
+                  usage();
+                  exit(0);
+              case 't':
+                *tab_out = atoi(optarg);
+                break;
+              case '?':
+                  fprintf(stderr, "Error: ");
+                  if (optopt == 't')
+                      fprintf(stderr, "option '-%c' requires an argument.\n",
+                              optopt);
+                  else
+                      fprintf(stderr, "unknown option '-%c'.\n", optopt);
+                  fprintf(stderr, "%s\n", HELP_MSG);
+                  exit(1);
+            }
+}
+
+
+int check_next(char* line, int count, char c)
+/* Check that next 'count' chars are equal to 'c' */
+{
+        for (int i = 0; i < count; ++i) {
+                if (line[i] != c)
                         return 0;
         }
         return 1;
 }
 
-int main(void)
+int main(int argc, char** argv)
 {
-        int i, len;
+        int i, len, tab;
         char line[MAX_LINE];
 
-        while ((len = get_line(line, MAX_LINE)) > 0) {
+        tab = 8;
+        parse_opts(argc, argv, &tab);
 
-                for (i = 0; i < len - TAB + 1; ++i) {
-                        if (line[i] == ' ' && check_next(line, i + 1, TAB - 1, ' ')) {
+        while ((len = get_line(line, MAX_LINE)) > 0) {
+                for (i = 0; i < len - tab + 1; ++i)
+                        if (line[i] == ' ' && check_next(line+i+1, tab-1, ' ')) {
                                 putchar('\t');
-                                i += TAB - 1;
+                                i += tab - 1;
                         } else
                                 putchar(line[i]);
-                }
-                for (; i < len; ++i)
-                        putchar(line[i]);
+                printf("%s", line + i);
         }
 
         return 0;
